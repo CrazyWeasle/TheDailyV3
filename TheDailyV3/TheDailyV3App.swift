702c010520a1e0b2fe86d986aa5a01d215178a92
jjarriel <dusk_lows_0n@icloud.com>
2026-03-05 09:41:02 -0700

    Initial commit: xcode boiler plate and .gitignore file

---
+++ TheDailyV3/TheDailyV3App.swift
@@ -0,0 +1,17 @@
+//
+//  TheDailyV3App.swift
+//  TheDailyV3
+//
+//  Created by Joe Jarriel on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TheDailyV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
